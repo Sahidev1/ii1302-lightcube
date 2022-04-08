45ce74a6f86bb6e8691a3a5014e2d4f8b1edbf34
ftajik <ftajik@kth.se>
2022-04-08 13:24:01 +0200

    added login and sign up components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Login from './View/login'
+import Register from './View/register';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> dummy </h1>
         <Login/>
+        <Register/>
       </header>
     </div>
   );
 }
-
 export default App;
--- src/View/register.js
+++ src/View/register.js
@@ -1,29 +1,34 @@
-function Login() {
+function Register() {
     return (
         <div>
             <div>
-                <h2>Log In To IoT Lightcube</h2>
+                <h2>Create Your Account</h2>
             </div>
             <div>
                 <table align="center">
                   <tbody>
                     <tr>
-                        <td align="center">User Name</td>
+                        <td align="center">Username</td>
                         <td>
                             <input type="text" placeholder="Enter your username"/>
                         </td>
                     </tr>
                     <tr>
-                        <td align="center">Password</td>
+                        <td align="center">Password</td>                            
                         <td>
                             <input type="password" placeholder="Enter your password"/>
                         </td>
                     </tr>
                     <tr>
+                        <td align="center">Confirm Password</td>
+                        <td>
+                            <input type="password" placeholder="Enter your password again"/>
+                        </td>
+                    </tr>
+                    <tr>
                         <td></td>
                         <td>
-                            <input type="submit" value="Login"/>
-                            <input type="submit" value="Register"/>
+                            <input type="submit" value="Create Account"/>
                         </td>
                     </tr>
                   </tbody>
@@ -32,4 +37,4 @@ function Login() {
         </div>
     )
 }
-export default Login
+export default Register
